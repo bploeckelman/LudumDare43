a8707d136f2dbda3a929eafae228def7af2babcb
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2018-11-27 21:50:21 -0600

    Initial commit

---
+++ desktop/src/lando/systems/ld43/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package lando.systems.ld43.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import lando.systems.ld43.LudumDare43;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LudumDare43(), config);
+	}
+}
