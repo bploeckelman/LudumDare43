8fd1f398708676c5133662dbdf695badff3a0c87
Jeffrey Hwang <jjhwang@wisc.edu>
2018-12-01 18:18:54 -0600

    Beeline behavior enemy

---
+++ core/src/lando/systems/ld43/entities/enemies/BeelineEnemy.java
@@ -0,0 +1,56 @@
+package lando.systems.ld43.entities.enemies;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import lando.systems.ld43.utils.Assets;
+
+import java.util.Random;
+
+public class BeelineEnemy extends Enemy {
+
+    private Vector2 velocity = new Vector2(-50, 0);
+
+    private float directionChangeFrequency = .5f;
+
+    private float minDirectionChangeAmount = 40f;
+    private float maxDirectionChangeAmount = 60f;
+
+    private static Random randomNumberGen = new Random();
+
+    float directionChangetimer = 0;
+
+    public BeelineEnemy(Assets assets, float x, float y) {
+        super(assets);
+        position.set(x, y);
+    }
+
+    @Override
+    public void update(float dt){
+        position.x += velocity.x * dt;
+        position.y += velocity.y * dt;
+
+        directionChangetimer += dt;
+        System.out.println("timer:" + directionChangetimer + " | frequency: " + directionChangeFrequency);
+        if(directionChangetimer >= directionChangeFrequency){
+            directionChangetimer -= directionChangeFrequency;
+            float directionChangeRange = maxDirectionChangeAmount - minDirectionChangeAmount;
+            // calculate a random change amount between the minimum and max
+            float directionChangeAmount = randomNumberGen.nextFloat() * directionChangeRange + minDirectionChangeAmount;
+            System.out.println("changeAmount: " + directionChangeAmount);
+            // apply the change amount to the velocity;
+            if(randomNumberGen.nextBoolean() || velocity.x > 0){
+                if(randomNumberGen.nextBoolean()){
+                    directionChangeAmount  = -directionChangeAmount;
+                }
+                velocity.y += directionChangeAmount;
+            } else {
+                velocity.x -= directionChangeAmount;
+            }
+        }
+    }
+
+    @Override
+    public void render(SpriteBatch batch){
+        batch.draw(assets.whitePixel, position.x - width/2, position.y - height/2, width, height);
+    }
+}
