8f0a7f1782dc89ba28ef11074580b71965ef8539
Jeffrey Hwang <jjhwang@wisc.edu>
2018-12-03 19:41:40 -0600

    endscreen edits

--- core/src/lando/systems/ld43/screens/EndScreen.java
+++ core/src/lando/systems/ld43/screens/EndScreen.java
@@ -22,12 +22,13 @@ public class EndScreen extends BaseScreen{
     private String heading = "Ulti-MEME SACRIFICE";
     private String theme = "Made for Ludum Dare 43:\nTheme: Sacrifices must be made";
     private String thanks = "Thanks for playing our game!";
-    private String developers = "Developed by:\nDoug Graham\nBrian Ploeckelman\nJeffrey Hwang\nBrandon Humboldt";
+    private String developers = "Developed by:\nDoug Graham\nBrian Ploeckelman\nJeffrey Hwang\nBrandon Humboldt\nLuke Bain";
     private String artists = "Art by:\nMatt Neumann\nLuke Bain";
     private String emotionalSupport = "Emotional Support:\nAsuka the Shiba\nNyquil\nEatStreet";
     //TODO add song title
     private String music = "Music by:\nTyler Pecora";
     private String libgdx = "Made with <3 and LibGDX";
+    private String disclaimer = "Disclaimer!!!\nNo animals were harmed in making of this game.";
 
     private Vector3 mousePos;
     private Vector2 suitPos;
@@ -99,14 +100,14 @@ public class EndScreen extends BaseScreen{
             batch.draw(playerSuit, suitPos.x, suitPos.y, 50, 25, 100, 50, 1, 1, suitRotation);
 
             assets.drawString(batch, heading, 0, hudCamera.viewportHeight - 10, Config.end_screen_text_color, .8f, assets.font, hudCamera.viewportWidth, Align.center);
-            assets.drawString(batch, theme, 0, hudCamera.viewportHeight - 60, Config.end_screen_text_color, .35f, assets.font, hudCamera.viewportWidth, Align.center);
-            assets.drawString(batch, developers, 0, hudCamera.viewportHeight - 120, Config.end_screen_text_color, .3f, assets.font, hudCamera.viewportWidth/2, Align.center);
-            assets.drawString(batch, emotionalSupport, 0, hudCamera.viewportHeight - 280, Config.end_screen_text_color, .3f, assets.font, hudCamera.viewportWidth/2, Align.center);
-            assets.drawString(batch, artists, hudCamera.viewportWidth/2, hudCamera.viewportHeight - 120, Config.end_screen_text_color, .3f, assets.font, hudCamera.viewportWidth/2, Align.center);
-            assets.drawString(batch, music, hudCamera.viewportWidth/2, hudCamera.viewportHeight - 220, Config.end_screen_text_color, .3f, assets.font, hudCamera.viewportWidth/2, Align.center);
-            assets.drawString(batch, libgdx, hudCamera.viewportWidth/2, hudCamera.viewportHeight - 300, Config.end_screen_text_color, .4f, assets.font, hudCamera.viewportWidth/2, Align.center);
-            assets.drawString(batch, thanks, 0, 200, Config.end_screen_text_color, .3f, assets.font, hudCamera.viewportWidth, Align.center);
-
+            assets.drawString(batch, theme, 0, hudCamera.viewportHeight - 80, Config.end_screen_text_color, .35f, assets.font, hudCamera.viewportWidth, Align.center);
+            assets.drawString(batch, developers, 0, hudCamera.viewportHeight - 160, Config.end_screen_text_color, .3f, assets.font, hudCamera.viewportWidth/2, Align.center);
+            assets.drawString(batch, emotionalSupport, 0, hudCamera.viewportHeight - 320, Config.end_screen_text_color, .3f, assets.font, hudCamera.viewportWidth/2, Align.center);
+            assets.drawString(batch, artists, hudCamera.viewportWidth/2, hudCamera.viewportHeight - 160, Config.end_screen_text_color, .3f, assets.font, hudCamera.viewportWidth/2, Align.center);
+            assets.drawString(batch, music, hudCamera.viewportWidth/2, hudCamera.viewportHeight - 320, Config.end_screen_text_color, .3f, assets.font, hudCamera.viewportWidth/2, Align.center);
+            assets.drawString(batch, libgdx, hudCamera.viewportWidth/2, hudCamera.viewportHeight - 380, Config.end_screen_text_color, .4f, assets.font, hudCamera.viewportWidth/2, Align.center);
+            assets.drawString(batch, disclaimer, 0, hudCamera.viewportHeight - 440, Config.end_screen_text_color, .5f, assets.font, hudCamera.viewportWidth, Align.center);
+            assets.drawString(batch, thanks, 0, hudCamera.viewportHeight - 550, Config.end_screen_text_color, .4f, assets.font, hudCamera.viewportWidth, Align.center);
             batch.draw(texturePointer, mousePos.x, mousePos.y - texturePointer.getRegionHeight());
         }
         batch.end();
